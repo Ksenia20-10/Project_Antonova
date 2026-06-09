b08c2ea7b8ebbe97befcb38eae1bf3c9a45e3a48
ksyusha <ksyushaantonova2008@gmail.com>
2026-06-09 22:25:50 +0300

    Add file

---
+++ PZ_8/PZ_8_1.py
@@ -0,0 +1,13 @@
+# Вариант 4. Задание 1
+# Найдите ключ с минимальным значением в sample_dict
+print("=== Задача 1 ===")
+
+sample_dict = {'Physics': 82, 'Math': 65, 'history': 75}
+
+# Исходный словарь
+print("Исходный словарь:", sample_dict)
+
+# Находим ключ с минимальным значением
+min_key = min(sample_dict, key=sample_dict.get)
+print("Ключ с минимальным значением:", min_key)
+print()
---
+++ PZ_8/PZ_8_2.py
@@ -0,0 +1,23 @@
+# Вариант 4. Задание 2
+# Дан словарь с четным количеством элементов. Найти суммы значений элементов
+# первой и второй половин с использованием функции.
+
+# Пример словаря с четным количеством элементов
+# (можно заменить на любой другой с четным числом элементов)
+even_dict = {'a': 10, 'b': 20, 'c': 30, 'd': 40}
+
+print("Исходный словарь:", even_dict)
+
+# Функция для подсчета суммы половин
+def sum_of_halves(dictionary):
+    items = list(dictionary.values())
+    mid = len(items) // 2
+    sum_first = sum(items[:mid])
+    sum_second = sum(items[mid:])
+    return sum_first, sum_second
+
+# Вызов функции
+first_sum, second_sum = sum_of_halves(even_dict)
+
+print("Сумма первой половины:", first_sum)
+print("Сумма второй половины:", second_sum)
